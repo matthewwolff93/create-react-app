82056cdc43b8e90adf9e66cc159c96dd93ca72f4
Matthew Wolff <matthew.wolff93@gmail.com>
2018-03-01 18:02:53 +1300

    Initial commit

---
+++ hello/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorldList from './HelloWorldList';
+
+import './App.css';
+
+const App = () => (
+	<div className="App">
+		<HelloWorldList />
+	</div>
+);
+
+export default App;
---
+++ hello/src/HelloWorldList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './HelloWorldList.css';
+
+import HelloWorld from './HelloWorld';
+
+class HelloWorldList extends Component {
+	render() {
+		return (
+			<div className="HelloWorldList">
+				<HelloWorld name="Jim" />
+				<HelloWorld name="Sally" />
+			</div>
+		);
+	}
+}
+
+export default HelloWorldList;
